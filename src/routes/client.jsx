6b1b0c471b8887508a3c89d4e92e73b86cb21db5
iwanttos1eep <79249933+iwanttos1eep@users.noreply.github.com>
2024-04-12 11:02:39 +0000

    v1.1.1

--- src/routes/client.jsx
+++ src/routes/client.jsx
@@ -1,45 +1,64 @@
 export default function Client() {
+
+
     return (
       
       <>
-    <div class="container">
-      <div class="navbar">
-        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="#">Fixed navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarCollapse">
-              <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
-              </ul>
-              <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button class="btn btn-outline-success" type="submit">Search</button>
-              </form>
-            </div>
-          </div>
-        </nav>
-        </div>
-        <div class="cont">
-        <h2>Виды услуг и направления</h2>
-          <div class="btn-group w-100" role="group" aria-label="Basic example">
-            <button type="button" class="btn btn-primary ">Лефт</button>
-            <button type="button" class="btn btn-primary ">Middle</button>
-            <button type="button" class="btn btn-primary ">Right</button>
-          </div>
-        </div>
-    </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
+  
+  <div class="container-fluid">
+    
+    <button
+      data-mdb-collapse-init
+      class="navbar-toggler"
+      type="button"
+      data-mdb-target="#navbarCenteredExample"
+      aria-controls="navbarCenteredExample"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i class="fas fa-bars"></i>
+    </button>
 
-        </>
+    
+    <div
+      class="collapse navbar-collapse justify-content-center"
+      id="navbarCenteredExample"
+    >
+      
+      <ul class="navbar-nav mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">ГЛАВНАЯ</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">О НАС</a>
+        </li>
+        
+      </ul>
+      
+    </div>
+    
+  </div>
+  
+</nav>
+      <h2>Виды услуг и направления</h2>
+
+<div class="large-buttons">
+  <div class="container">
+    <div class="buttons-holder">
+
+      <div class="d-grid gap-2">
+      <button type="button" class="btn btn-primary">Primary</button>
+      </div>
+      <div class="d-grid gap-2 col-6">
+      <button type="button" class="btn btn-primary">Primary</button>
+      </div>
+      <div class="d-grid gap-2 col-6">
+      <button type="button" class="btn btn-primary">Primary</button>
+      </div>
+    </div>
+  </div>
+</div>
+      </>
     )
 }
