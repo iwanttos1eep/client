114c0eab778a957c603bf3358e13a6f287cbd678
Русаков Никита Станиславович <n.rusakov@unicon.ru>
2024-04-18 20:32:16 +0300

    build: added material-ui

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
   <link href="sign-in.css" rel="stylesheet" />;
