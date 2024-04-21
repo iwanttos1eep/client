2f4ccf5543b8aa9d463125c26f42ed92d75da8ac
Русаков Никита Станиславович <n.rusakov@unicon.ru>
2024-04-21 19:38:53 +0300

    fix[Header]: позиционирование хеддера

--- src/components/feature/Header/index.tsx
+++ src/components/feature/Header/index.tsx
@@ -23,28 +23,31 @@ interface IHeaderProps {
 const Header = (props: IHeaderProps) => {
   const { navItems, headerLogo } = props;
   return (
-    <AppBar component="div" position="relative">
-      <Toolbar>
-        <Container>
-          <Stack direction="row" justifyContent="space-between" width="100%">
-            <Button sx={{ color: '#fff' }} onClick={headerLogo.onClick}>
-              {headerLogo.title}
-            </Button>
-            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-              {navItems.map((item, index) => (
-                <Button
-                  key={index}
-                  sx={{ color: '#fff' }}
-                  onClick={item.onClick}
-                >
-                  {item.title}
-                </Button>
-              ))}
-            </Box>
-          </Stack>
-        </Container>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar component="div">
+        <Toolbar>
+          <Container>
+            <Stack direction="row" justifyContent="space-between" width="100%">
+              <Button sx={{ color: '#fff' }} onClick={headerLogo.onClick}>
+                {headerLogo.title}
+              </Button>
+              <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                {navItems.map((item, index) => (
+                  <Button
+                    key={index}
+                    sx={{ color: '#fff' }}
+                    onClick={item.onClick}
+                  >
+                    {item.title}
+                  </Button>
+                ))}
+              </Box>
+            </Stack>
+          </Container>
+        </Toolbar>
+      </AppBar>
+      <Box width="100%" height="64px"></Box>
+    </>
   );
 };
 
