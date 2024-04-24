b76b4e80706f21ec581dd85b04a1cb628920971e
Русаков Никита Станиславович <n.rusakov@unicon.ru>
2024-04-25 00:07:55 +0300

    feat: added AdminPage

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,15 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import HomePage from "./pages/HomePage";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
+import AdminPAge from './pages/AdminPage';
 
 const RouterWrapper = () => {
   return (
     <Routes>
       <Route path="/" Component={HomePage} />
       <Route path="/login" Component={LoginPage} />
+      <Route path="/admin" Component={AdminPAge} />
     </Routes>
   );
 };
---
+++ src/utils/stringAvatar.tsx
@@ -0,0 +1,10 @@
+import { stringToColor } from './stringToColor';
+
+export const stringAvatar = (name: string) => {
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+  };
+};
