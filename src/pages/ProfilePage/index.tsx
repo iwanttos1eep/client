b4de1b05a082b002b45d2313e12e7846178f5234
Русаков Никита Станиславович <n.rusakov@unicon.ru>
2024-05-28 20:35:00 +0300

    feat[ProfilePage]: showing warning and success getting user

--- src/pages/ProfilePage/index.tsx
+++ src/pages/ProfilePage/index.tsx
@@ -1,50 +1,86 @@
 import {
+  Alert,
   Avatar,
   Card,
   CardContent,
   CardHeader,
   Container,
+  Snackbar,
   Stack,
   Typography,
 } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/feature/Header';
 import { stringAvatar } from '../../utils/stringAvatar';
 import Footer from '../../components/feature/Footer';
-import { useAppSelector } from '../../hooks/store';
-import { selectAuth } from '../../store/slice/authSlice';
+import { useAppDispatch, useAppSelector } from '../../hooks/store';
+import { logOut, selectAuth } from '../../store/slice/authSlice';
 import { useGetUserByIdQuery } from '../../store/api/userApi';
+import { useNavigate } from 'react-router-dom';
+import { ERoles } from '../../interfaces/roles';
 
 const ProfilePage = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   const userAuthData = useAppSelector(selectAuth);
-  const { data: user } = useGetUserByIdQuery(
-    { userId: userAuthData.id ?? 0, token: userAuthData.token ?? '' },
-    {
-      skip: !userAuthData.id && !userAuthData.token,
-    },
-  );
+  const {
+    data: user,
+    isError: isUserErrorQuery,
+    isSuccess: isUserSuccessQuery,
+  } = useGetUserByIdQuery({
+    userId: userAuthData.id ?? 0,
+    token: userAuthData.token ?? '',
+  });
+  const role = user?.roles ? user?.roles[0] : undefined;
+  const [isUserError, setUserError] = useState<boolean>(false);
+  const [isUserSuccess, setUserSuccess] = useState<boolean>(false);
 
-  console.log(user);
+  useEffect(() => {
+    setUserError(isUserErrorQuery);
+  }, [isUserErrorQuery]);
+
+  useEffect(() => {
+    setUserSuccess(isUserSuccessQuery);
+  }, [isUserSuccessQuery]);
+
+  useEffect(() => {
+    if (isUserError) {
+      navigate('/login');
+    }
+  }, [isUserError]);
 
   return (
     <Stack>
       <Header
         headerLogo={{
           title: 'Главная',
-          onClick() {},
+          onClick() {
+            if (role?.name === ERoles.ROLE_USER) {
+              navigate('/');
+            } else if (role?.name === ERoles.ROLE_ADMIN) {
+              navigate('/admin');
+            } else navigate('/trainer');
+          },
         }}
         navItems={[
           {
             title: 'О нас',
-            onClick() {},
+            onClick() {
+              navigate('/about');
+            },
           },
           {
             title: 'Мой профиль',
-            onClick() {},
+            onClick() {
+              navigate('/profile');
+            },
           },
           {
             title: 'выйти',
-            onClick() {},
+            onClick() {
+              dispatch(logOut());
+              navigate('/login');
+            },
           },
         ]}
       />
@@ -80,7 +116,7 @@ const ProfilePage = () => {
                   <Typography>
                     Взятый инвентарь:{' '}
                     {user?.inventoryList?.map((inventory) => (
-                      <span>{inventory.name}</span>
+                      <span key={inventory.id}>{inventory.name}</span>
                     ))}
                   </Typography>
                 </Stack>
@@ -90,6 +126,28 @@ const ProfilePage = () => {
         </Container>
       </Stack>
       <Footer />
+      <Snackbar
+        open={isUserError}
+        autoHideDuration={3000}
+        onClose={() => {
+          setUserError(false);
+        }}
+      >
+        <Alert severity="error" variant="filled">
+          Ошибка
+        </Alert>
+      </Snackbar>
+      <Snackbar
+        open={isUserSuccess}
+        autoHideDuration={3000}
+        onClose={() => {
+          setUserSuccess(false);
+        }}
+      >
+        <Alert severity="success" variant="filled">
+          Успех!
+        </Alert>
+      </Snackbar>
     </Stack>
   );
 };
